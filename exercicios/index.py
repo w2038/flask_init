8685708adf97541c71ecbf82daf85c5deea8aec5
w2038 <w4gn3r2038@gmail.com>
2023-06-16 18:42:45 -0300

    exercicios

---
+++ exercicios/index.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/search")
+def search():
+    keyword = request.args.get('q')
+    category = request.args.get('category')
+    
+    if keyword:
+        return f"Reazando uma busca po '{keyword}' na categoria '{category}'."
+    else:
+        return "Por favor, forneça uma palavra-chave para fazer a busca."
+
+if __name__ == "__main__":
+    app.run()
