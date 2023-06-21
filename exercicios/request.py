f924a4e7d6d92b3cef705a9fc57acd811f281504
w2038 <w4gn3r2038@gmail.com>
2023-06-21 01:22:12 -0300

    .

---
+++ exercicios/request.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    age = request.form.get('age')
+    
+    return render_template("result.html", name=name, age=age)
+
+
+if __name__ == "__main__":
+    app.run()
